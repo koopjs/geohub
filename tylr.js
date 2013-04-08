30ccbf5e8418ab7e0f008678139877cb93684a4f
chelm <christopher_helm@esri.com>
2013-04-07 21:04:49 -0600

    adding bin file and new structure

--- tylr.js
+++ tylr.js
@@ -1,58 +1,110 @@
-#!/usr/bin/env node
+var nfs = require('node-fs'),
+  async = require('async');
 
-var fs = require('fs');
+module.exports = {
 
-var argv = require('optimist')
-    .usage('Usage: $0 -f [string] -d [string] -l [string]')
-    .demand(['f','d'])
-    .default('l', '0,5')
-    .argv;
+  tyle: function( file, dir, levels ){
+    var self = this;
 
+    console.log( file, dir, levels );
 
+    if ( nfs.existsSync( file ) ) {
+  
+      nfs.readFile( file, function( err, data ){
+        console.log('data', data)
+        var features = JSON.parse(data).features;
+  
+        features.forEach(function( f ){
+          var ll = self.center( f.geometry );
+          console.log( ll );
+  
+          var z = parseInt( levels[0] );
+  
+          while ( z <= levels[ 1 ] ) {
+            var xyz = self.location( ll[1], ll[0], z )
+            self.q.push({ dir: dir, x: xyz.x, y: xyz.y, z: xyz.z, feature: f }, function (err) {} );
+            z++;
+          }
+        });
+      });
+    } else {
+      console.log('Can\'t find input geojson file');
+    }
 
-function tyle( file, dir, levels ){
+  },
 
-  if ( !fs.existsSync( dir ) ) fs.mkdirSync( dir );
+  center: function( geom ){
 
-  fs.readFile( file, function( err, data ){
-    var features = JSON.parse(data).features;
-    
-    features.forEach(function( f ){
-      /*console.log( center( ( f.geometry.coordinates.length > 1 ) 
-        ? f.geometry.coordinates[0] : f.geometry.coordinates ) );*/
-      var ll = center( f.geometry.coordinates[0] )
-      console.log(ll)
+    var minx, miny, maxx, maxy;
 
-      //console.log(file, dir, levels);
+    function adjust( c ){
+      var len = c.length;
+      while ( len-- ){
+        var p = c[len];
+        minx = ( p[0] < minx ) ? p[0] : minx;
+        miny = ( p[1] < miny ) ? p[1] : miny;
+        maxx = ( p[0] > maxx ) ? p[0] : maxx;
+        maxy = ( p[1] > maxy ) ? p[1] : maxy;
+      }
+    }
 
-      var z = parseInt(levels[0]);
+    if (geom.type == 'MultiPolygon'){
+      var coords = geom.coordinates[0];
+      minx = coords[0][0][0],
+        miny = coords[0][0][1],
+        maxx = coords[0][0][0],
+        maxy = coords[0][0][1];
+      geom.coordinates.forEach(function( polygon ){
+        adjust( polygon[0] );
+      });
+    } else {
+      var coords = geom.coordinates[0];
+      minx = coords[0][0],
+        miny = coords[0][1],
+        maxx = coords[0][0],
+        maxy = coords[0][1];
+      
+      adjust( geom.coordinates[0] ); 
+    }
+    return [ (maxx + minx) / 2, (maxy + miny) / 2 ];
+  },
 
-      while ( z <= levels[ 1 ] ) {
-        console.log('\t', locCoord(ll[1], ll[0], z));
-        z++
+  location: function( lat, lon, zoom ) {
+    var lon_rad = lon * (Math.PI / 180),
+      lat_rad = lat * (Math.PI / 180),
+      n = Math.pow(2.0, zoom);
+  
+    var tileX = Math.floor( ( ( lon + 180 ) / 360 ) * n );
+    var tileY = Math.floor( ( 1 - ( Math.log( Math.tan( lat_rad ) + 1.0 / Math.cos( lat_rad ) ) / Math.PI ) ) * n / 2.0 );
+  
+    return { x : tileX, y: tileY, z: zoom };
+  },
+
+  q: async.queue(function (task, callback) {
+
+    var p = [task.dir, task.z, task.x].join('/');
+    var file = p + '/' + task.y + '.png';
+
+    nfs.mkdir( p, '0777', true, function(){
+      if ( !nfs.existsSync( file ) ) {
+        nfs.writeFile( file, JSON.stringify({ type:'FeatureCollection', features: [ task.feature ] }));
+        callback();
+      } else {
+        nfs.readFile( file, function(err, data){
+          var json = JSON.parse(data.toString());
+          json.features.push( task.feature );
+
+          nfs.writeFile( file, JSON.stringify( json ));
+          callback();
+        })
       }
     });
-  });
 
-}
+  }, 2)
 
-function center( coords ){
-    //coords = coords[0];
-    var minx = coords[0][0],
-      miny = coords[0][1],
-      maxx = coords[0][0],
-      maxy = coords[0][1];
 
-    var len = coords[0].length;
-    /*while ( len-- ){
-      var c = coords[0][len];
-      minx = ( c[0] < minx ) ? c[0] : minx;
-      miny = ( c[1] < miny ) ? c[1] : miny;
-      maxx = ( c[0] > maxx ) ? c[0] : maxx;
-      maxy = ( c[1] > maxy ) ? c[1] : maxy;
-    }*/
-    return [ (maxx + minx) / 2, (maxy + miny) / 2 ];
-}
+};
+  
 
 /*function centroid( coords ){
   var area = 0;
@@ -79,17 +131,3 @@ function center( coords ){
   f = area * 3;
   return [ x/f + coords[0][0][0], y/f + coords[0][0][1] ];
 }*/
-
-function locCoord(lat, lon, zoom){
-  var lon_rad = lon * (Math.PI / 180),
-    lat_rad = lat * (Math.PI / 180),
-    n = Math.pow(2.0, zoom);
-
-  var tileX = Math.floor( ( ( lon + 180 ) / 360 ) * n );
-  var tileY = Math.floor( ( 1 - ( Math.log( Math.tan( lat_rad ) + 1.0 / Math.cos( lat_rad ) ) / Math.PI ) ) * n / 2.0 );
-
-  return { x : tileX, y: tileY, z: zoom };
-}
-
-
-tyle( argv.f, argv.d, argv.l.split(',') );
