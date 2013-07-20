02a7aa3adc32205c96e6b3f07c9d425da3a9a310
chelm <christopher_helm@esri.com>
2013-07-19 20:47:49 -0600

    github geojson is working

--- geohub.js
+++ geohub.js
@@ -1,6 +1,29 @@
-var request = require('request');
+var request = require('request'),
+  GitHubApi = require('github');
+
+var github = new GitHubApi({
+  version: "3.0.0",
+  timeout: 5000
+}); 
 
 module.exports = {
+
+  // scan repo for "geojson files"
+  repo: function( user, repo, path, callback ){
+    var url = 'https://raw.github.com/'+ user + '/' + repo + '/master/' + path + '.geojson';
+    request(url, function( error, response, body ){
+      if (!error && response.statusCode == 200) {
+        var json = JSON.parse( body );
+        try {
+          if (json.type && json.type == 'FeatureCollection'){
+            callback( null, json );
+          }
+        } catch (e){
+          callback('Error: could not parse file contents' + e, null);
+        }
+      }
+    });
+  },
 
   gist: function( id, callback ){
     var url = 'https://api.github.com/gists/' + id;
