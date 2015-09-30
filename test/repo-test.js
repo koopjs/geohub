520083eed653733d8bc042e8ea89a187685a4a7e
Nate Goldman <nate@ngoldman.me>
2015-09-30 13:08:05 -0700

    :white_check_mark: add token to test, improve multiple file repo test

--- test/repo-test.js
+++ test/repo-test.js
@@ -3,9 +3,10 @@ var Geohub = require('../')
 var user = 'chelm'
 var repo = 'grunt-geo'
 var path = 'forks'
+var token = process.env.GITHUB_TOKEN
 
 test('When requesting geojson from a repo with geojson', function (t) {
-  Geohub.repo(user, repo, path, null, function (err, data) {
+  Geohub.repo(user, repo, path, token, function (err, data) {
     t.error(err, 'does not error')
     t.ok(data, 'data exists')
     t.equal(data.type, 'FeatureCollection', 'data is a FeatureCollection')
@@ -15,7 +16,7 @@ test('When requesting geojson from a repo with geojson', function (t) {
 })
 
 test('When checking the sha of a file', function (t) {
-  Geohub.repoSha(user, repo, 'forks.geojson', null, function (err, data) {
+  Geohub.repoSha(user, repo, 'forks.geojson', token, function (err, data) {
     t.error(err, 'does not error')
     t.ok(data, 'data exists')
     t.equal(typeof data, 'string', 'data is a string')
@@ -24,18 +25,25 @@ test('When checking the sha of a file', function (t) {
 })
 
 test('When only passing in a user/repo', function (t) {
-  Geohub.repo(user, repo, null, null, function (err, data) {
+  Geohub.repo(user, repo, null, token, function (err, data) {
     t.error(err, 'does not error')
     t.ok(data, 'data exists')
     t.equal(data.length, 3, 'data has length of 3')
-    t.equal(data[0].name, 'forks.geojson', 'first object in data has name of forks.geojson')
-    t.ok(data[0].sha, 'first object in data has sha property')
+    t.ok(data[0].sha, 'object 1 has sha property')
+    t.ok(data[1].sha, 'object 2 has sha property')
+    t.ok(data[2].sha, 'object 3 has sha property')
+    t.equal(data[0].type, 'FeatureCollection', 'object 1 is FeatureCollection')
+    t.equal(data[1].type, 'FeatureCollection', 'object 2 is FeatureCollection')
+    t.equal(data[2].type, 'FeatureCollection', 'object 3 is FeatureCollection')
+    t.ok(data[0].features.length > 0, 'object 1 has features')
+    t.ok(data[1].features.length > 0, 'object 2 has features')
+    t.ok(data[2].features.length > 0, 'object 3 has features')
     t.end()
   })
 })
 
 test('When only passing in a user/repo with a path that is a dir', function (t) {
-  Geohub.repo(user, repo, 'samples', null, function (err, data) {
+  Geohub.repo(user, repo, 'samples', token, function (err, data) {
     t.error(err, 'does not error')
     t.ok(data, 'data exists')
     t.equal(data.length, 7, 'data has length of 7')
